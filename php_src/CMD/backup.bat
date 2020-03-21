f5fa326d9e82f83e6769c3b3f0d523bfd1ed5232
sek sitha <sitha@geekho.asia>
2020-03-21 17:29:51 +0700

    fix push empty mysql folder

--- php_src/CMD/backup.bat
+++
@@ -1,9 +0,0 @@
-@echo OFF
-::httpd.exe -k install -n "Apache2.4"
-::shutdown -r /t 3
-::shutdown -t 0 -r -f
-::echo heloworld
-C:\xampp\mysql\bin\mysqldump -h localhost -u root -pworkshop2014 --databases krukhmerdata > "C:\xampp\htdocs\kk_database_backup.sql"
-C:\xampp\mysql\bin\mysql repair table oms_sale
-
-
--- php_src/CMD/cmd.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-ini_set('max_execution_time', 300); //300 seconds = 5 minutes
-echo $_POST['cmd'];
-exec($_POST['cmd']);
-
-?>
-
